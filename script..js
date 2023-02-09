33f7076552985972e1eda9361a67d0597bd2ed3c
Chyniece Matthews <cfmatthews2@gmail.com>
2023-02-08 22:32:36 -0600

    Update

---
+++ script..js
@@ -0,0 +1,13 @@
+//add at least 3 dynamic components to your webpage
+//contact box 
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+  if (mySrc === "https://images.squarespace-cdn.com/content/v1/5531eb6de4b0c759b385ac75/1560479909116-HN4PHGEH8P3URAE0OAQ6/Screen+Shot+2019-06-13+at+6.26.02+PM.png?format=1000w") {
+    myImage.setAttribute("src", "bacon.jfif");
+  } else {
+    myImage.setAttribute("src", "https://images.squarespace-cdn.com/content/v1/5531eb6de4b0c759b385ac75/1560479909116-HN4PHGEH8P3URAE0OAQ6/Screen+Shot+2019-06-13+at+6.26.02+PM.png?format=1000w");
+  }
+};
